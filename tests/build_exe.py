dc8652eef7c8a75325e67d7dc7a691dc76374530
xaoyaoo <xaoyaoo@qq.com>
2024-01-24 20:30:35 +0800

    更新

--- tests/build_exe.py
+++ tests/build_exe.py
@@ -68,35 +68,24 @@ coll = COLLECT(exe,
 '''
 # 创建文件夹
 os.makedirs("dist", exist_ok=True)
-# 将代码写入文件
-# with open("dist/tmp.py", "w", encoding="utf-8") as f:
-#     f.write(code.strip())
-shutil.copy("../pywxdump_mini/simplify_wx_info.py", "dist/tmp.py")  # 复制代码
-
-# base64_to_image(ico_base64, "dist/icon.png")  # 将 base64 转换为图片
+# 当前文件所在目录
+current_path = os.path.dirname(os.path.abspath(__file__))
+shutil.copy(os.path.join(os.path.dirname(current_path), "pywxdump_mini", "simplify_wx_info.py"), "dist/tmp.py")  # 复制代码
 shutil.copy("icon.ico", "dist/icon.ico")  # 复制图标
 
-# 获取安装包的路径
-package_path = site.getsitepackages()
-if package_path:
-    package_path = package_path[1]  # 假设取第一个安装包的路径
+require_path = os.path.join(os.path.dirname(current_path), "requirements.txt")  # requirements.txt 路径
+with open(require_path, "r", encoding="utf-8") as f:
+    hidden_imports = f.read().splitlines()
+hidden_imports = [i for i in hidden_imports if i not in ["setuptools", "wheel"]]  # 去掉setuptools、wheel
 
-    current_path = os.path.dirname(os.path.abspath(__file__))  # 当前文件所在路径
-    require_path = os.path.join(os.path.dirname(current_path), "requirements.txt")  # requirements.txt 路径
-    with open(require_path, "r", encoding="utf-8") as f:
-        hidden_imports = f.read().splitlines()
-    hidden_imports = [i for i in hidden_imports if i not in ["setuptools", "wheel"]]  # 去掉setuptools、wheel
+# 生成 spec 文件
+spec_content = spec_content.format(hidden_imports=hidden_imports)
+spec_file = os.path.join("dist", "pywxdump.spec")
+with open(spec_file, 'w', encoding="utf-8") as f:
+    f.write(spec_content.strip())
 
-    # 生成 spec 文件
-    spec_content = spec_content.format(hidden_imports=hidden_imports)
-    spec_file = os.path.join("dist", "pywxdump.spec")
-    with open(spec_file, 'w', encoding="utf-8") as f:
-        f.write(spec_content.strip())
+# 执行打包命令
+cmd = f'pyinstaller --clean  --distpath=dist {spec_file}'
+print(cmd)
+# os.system(cmd)
 
-    # 执行打包命令
-    cmd = f'pyinstaller --clean  --distpath=dist {spec_file}'
-    print(cmd)
-    # os.system(cmd)
-
-else:
-    print("未找到安装包路径")
