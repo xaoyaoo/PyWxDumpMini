915c09121aa8290d31182e70288c286f498d19f9
xaoyaoo <xaoyaoo@qq.com>
2024-01-29 12:10:41 +0800

    添加使用说明

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-#
+# -------------------------------------------------------------------------------
+# Name:         test.py
+# Description:  
+# Author:       xaoyaoo
+# Date:         2024/01/29
+# -------------------------------------------------------------------------------
+from pywxdump_mini import read_info
+
+a = read_info(is_logging=True, is_save=True)
+print(a)
+
+if __name__ == '__main__':
+    pass
